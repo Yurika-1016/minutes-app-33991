cfdda951e797fe8d61a3383d9e4efe849470f518
Yurika-1016 <cix11103@nitech.jp>
2021-02-09 22:22:49 +0900

    first commit

--- app/controllers/minutes_controller.rb
+++ app/controllers/minutes_controller.rb
@@ -1,5 +1,7 @@
 class MinutesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_minute, only: [:show, :edit, :update, :destroy]
+  before_action :not_user_permitted, only: [:edit, :destroy]
 
   def index
     @minutes = Minute.all.order(id: 'DESC')
@@ -18,10 +20,37 @@ class MinutesController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @minute.update(minute_params)
+      redirect_to minute_path(@minute.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @minute.destroy
+    redirect_to root_path
+  end
+
   private
 
   def minute_params
     params.require(:minute).permit(:title, :project_name, :participate_member, :meeting_date, :all_complete_date, :content,
                                    images: []).merge(user_id: current_user.id)
+  end
+
+  def set_minute
+    @minute = Minute.find(params[:id])
+  end
+
+  def not_user_permitted
+    redirect_to root_path unless @minute.user_id == current_user.id
   end
 end
